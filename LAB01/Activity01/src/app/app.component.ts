449929171dfc3e66677c700cc9c7858332f83f46
MadridRichard <20235694@s.ubaguio.edu>
2025-02-05 12:52:18 +0800

    hehehe

--- LAB01/Activity01/src/app/app.component.ts
+++ LAB01/Activity01/src/app/app.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { MainContentComponent } from './main-content/main-content.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HeaderComponent, FooterComponent, MainContentComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
