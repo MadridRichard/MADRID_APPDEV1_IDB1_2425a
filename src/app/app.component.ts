0235818fdc20e44e0e8d72375343c53d223430bb
MadridRichard <20235694@s.ubaguio.edu>
2025-02-12 13:26:21 +0800

    THIS IS MY SECOND LABORATORY ACTIVITY

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { UserProfileComponent } from './user-profile/user-profile.component';
+import { ContactFormComponent } from './contact-form/contact-form.component';
+import { HomepageComponent } from './homepage/homepage.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, UserProfileComponent, ContactFormComponent, HomepageComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
